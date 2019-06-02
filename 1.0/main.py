06b7e4528bd819f0bcdee729e9ccf70337c13b33
tdude92 <41596427+tdude92@users.noreply.github.com>
2019-06-02 00:17:28 -0400

    Stored all deetrimino sprites in variables

--- 1.0/main.py
+++ 1.0/main.py
@@ -10,20 +10,53 @@ sprite_sheet = pyglet.resource.image("deetriminos.png")
 
 # Deetriminos:
 """
-Each image of a deetrimino is stored in a tuple that 
+Each image of a deetrimino is stored in a list that 
 contains all possible orientations of that deetrimino.
 """
-LIGHT_BLUE = (
+deetrimino_I = [
     sprite_sheet.get_region(x = 0, y = 960, width = 128, height = 32),
     sprite_sheet.get_region(x = 0, y = 800, width = 32, height = 128)
-)
+]
+
+deetrimino_J = [
+    sprite_sheet.get_region(x = 160, y = 928, width = 96, height = 64),
+    sprite_sheet.get_region(x = 160, y = 800, width = 64, height = 96),
+    sprite_sheet.get_region(x = 160, y = 704, width = 96, height = 64),
+    sprite_sheet.get_region(x = 160, y = 592, width = 64, height = 96)
+]
+
+deetrimino_L = [
+    sprite_sheet.get_region(x = 288, y = 928, width = 96, height = 64),
+    sprite_sheet.get_region(x = 288, y = 800, width = 64, height = 96),
+    sprite_sheet.get_region(x = 288, y = 704, width = 96, height = 64),
+    sprite_sheet.get_region(x = 288, y = 592, width = 64, height = 96)
+]
+
+deetrimino_O = [
+    sprite_sheet.get_region(x = 0, y = 496, width = 64, height = 64)
+]
+
+deetrimino_S = [
+    sprite_sheet.get_region(x = 96, y = 496, width = 96, height = 64),
+    sprite_sheet.get_region(x = 96, y = 368, width = 64, height = 96),
+]
+
+deetrimino_Z = [
+    sprite_sheet.get_region(x = 224, y = 496, width = 96, height = 64),
+    sprite_sheet.get_region(x = 224, y = 368, width = 64, height = 96),
+]
+
+deetrimino_T = [
+    sprite_sheet.get_region(x = 0, y = 272, width = 96, height = 64),
+    sprite_sheet.get_region(x = 0, y = 144, width = 64, height = 96),
+    sprite_sheet.get_region(x = 128, y = 272, width = 96, height = 64),
+    sprite_sheet.get_region(x = 128, y = 144, width = 64, height = 96)
+]
 
 
 @window.event
 def on_draw():
     window.clear()
-    LIGHT_BLUE[0].blit(0, 0)
-    LIGHT_BLUE[1].blit(160, 0)
 
 window.push_handlers(pyglet.window.event.WindowEventLogger()) # This prints every event onto the console.
 pyglet.app.run()
